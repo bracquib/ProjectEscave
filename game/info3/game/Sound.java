1efb3d802ddee89b8bc68f6090a918d2d95ff141
Ana Gelez <ana@gelez.xyz>
2022-06-02 11:13:28 +0200

    Format the whole project

--- game/info3/game/Sound.java
+++ game/info3/game/Sound.java
@@ -29,19 +29,19 @@ import info3.game.sound.AudioPlayer;
 import info3.game.sound.AudioPlayerListener;
 
 public class Sound {
-  GameCanvas m_canvas;
-  String m_name;
-  String m_filename;
+	GameCanvas m_canvas;
+	String m_name;
+	String m_filename;
 
-  Sound(GameCanvas canvas) {
-    m_canvas = canvas;
-  }
+	Sound(GameCanvas canvas) {
+		m_canvas = canvas;
+	}
 
-  public void load(String name, String filename, long duration, float volume) throws IOException {
-    m_name = name;
-    m_filename = filename;
-    File file = new File(filename);
-    FileInputStream fis = new FileInputStream(file);
-    m_canvas.playSound(name, fis, duration,volume);
-  }
+	public void load(String name, String filename, long duration, float volume) throws IOException {
+		m_name = name;
+		m_filename = filename;
+		File file = new File(filename);
+		FileInputStream fis = new FileInputStream(file);
+		m_canvas.playSound(name, fis, duration, volume);
+	}
 }
