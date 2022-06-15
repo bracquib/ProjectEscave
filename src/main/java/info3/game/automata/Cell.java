f14e5963d0f42ccea15779fa5092e84124ecc800
Benjamin Bracquier <benjamin.bracquier@etu.univ-grenoble-alpes.fr>
2022-06-15 11:03:52 +0200

    3eme commit

--- src/main/java/info3/game/automata/Cell.java
+++ src/main/java/info3/game/automata/Cell.java
@@ -3,33 +3,17 @@ package info3.game.automata;
 import info3.game.entities.Entity;
 
 public class Cell implements ICondition {
-	@Override
-	public boolean eval(Entity e, Direction d) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	Direction d;
+	Category c;
 
-	@Override
-	public boolean eval(Entity e, int touche) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean eval(Entity e, Direction d, Category c) {
-
-		return e.getBehaviour().cell(e, d, c);
-	}
-
-	@Override
-	public boolean eval(Entity e, Category c, Direction d) {
-		// TODO Auto-generated method stub
-		return false;
+	public void cell(Direction d, Category c) {
+		this.d = d;
+		this.c = c;
 	}
 
 	@Override
 	public boolean eval(Entity e) {
-		// TODO Auto-generated method stub
-		return false;
+		return e.getBehaviour().cell(e, d, c);
+
 	}
 }
--- src/main/java/info3/game/automata/MyDir.java
+++ src/main/java/info3/game/automata/MyDir.java
@@ -4,34 +4,16 @@ import info3.game.entities.Entity;
 
 public class MyDir implements ICondition {
 
+	Direction d;
+
+	public void myDir(Direction d) {
+		this.d = d;
+	}
+
 	@Override
 	public boolean eval(Entity e) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean eval(Entity e, Direction d) {
-
 		return e.getBehaviour().myDir(e, d);
-	}
 
-	@Override
-	public boolean eval(Entity e, int touche) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean eval(Entity e, Direction d, Category c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean eval(Entity e, Category c, Direction d) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
