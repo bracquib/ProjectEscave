f3e91e40f1adc797857406bd91d78195b9974f6e
Aiveriwhair <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-06-20 19:39:58 +0200

    init mobSpawn

---
+++ src/main/java/info3/game/MobSpawn.java
@@ -0,0 +1,17 @@
+package info3.game;
+
+import java.util.ArrayList;
+
+import info3.game.entities.Player;
+
+public class MobSpawn {
+	private final int MAXENTITY = 30;
+
+	private final int MIN_SPAWN_DISTANCE = 50;
+	private final int MAX_SPAWN_DISTANCE = 70;
+
+	public static void Spawn() {
+		ArrayList<Player> players = Model.getPlayers();
+
+	}
+}
