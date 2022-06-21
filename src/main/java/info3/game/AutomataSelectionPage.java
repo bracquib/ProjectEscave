ec153cd289e1f8308424670786cf1e5be02381be
Aiveriwhair <aiveriwhair@gmail.com>
2022-06-21 17:48:22 +0200

    init Automata selection

---
+++ src/main/java/info3/game/AutomataSelectionPage.java
@@ -0,0 +1,16 @@
+package info3.game;
+
+import javax.swing.JFrame;
+
+public class AutomataSelectionPage extends JFrame {
+	public final String TITLE = "Association des automates";
+
+	public AutomataSelectionPage() {
+		super();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	public static void main(String args[]) throws Exception {
+	}
+}
