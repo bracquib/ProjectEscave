93cd160b23de36bf7467f5184666daffe66b7cbd
Ana Gelez <ana@gelez.xyz>
2022-06-22 14:51:38 +0200

    Autres images pour le menu + chargement depuis le dossier resources

--- src/main/java/info3/game/menu/LoadSave.java
+++ src/main/java/info3/game/menu/LoadSave.java
@@ -1,6 +1,8 @@
 package info3.game.menu;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,15 +15,17 @@ public class LoadSave {
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
-		InputStream is = LoadSave.class.getResourceAsStream("UI/" + fileName);
+		InputStream is = null;
 		try {
+			is = new FileInputStream(new File("src/main/resources/UI/" + fileName));
 			img = ImageIO.read(is);
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				is.close();
+				if (is != null) {
+					is.close();
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
