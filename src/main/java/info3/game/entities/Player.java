1d78512c1e401e2afe2d46aef87e39600a4c53c6
Benjamin Bracquier <benjamin.bracquier@etu.univ-grenoble-alpes.fr>
2022-06-22 14:58:20 +0200

    Baisse de la satiété et de la soif degradation
    On gagne de la nourriture quand on tue des monstres

--- src/main/java/info3/game/entities/Player.java
+++ src/main/java/info3/game/entities/Player.java
@@ -16,6 +16,7 @@ public class Player extends RigidBody {
 	private float thirstPoints;
 	private float maxThirst = 100;
 	private Inventory inventory;
+	private int compt;
 
 	public Player(LocalController c, PlayerColor color, Vec2 pos, boolean local, int points) {
 		super(1, c, points);
@@ -39,6 +40,11 @@ public class Player extends RigidBody {
 	@Override
 	public void tick(long el) {
 		super.tick(el);
+		compt++;
+		if (compt % 10000 == 0) {
+			this.hungerPoints -= 5;
+			this.thirstPoints -= 5;
+		}
 		float curXSpeed = this.getSpeed().getX();
 		if (Math.abs(curXSpeed) > 5) {
 			if (curXSpeed < 0) {
