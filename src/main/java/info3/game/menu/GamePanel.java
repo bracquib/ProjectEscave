3fc3ead92fc9047083098e727b78b21ba129b963
LePutch <pucci.jeremy@gmail.com>
2022-06-22 18:46:23 +0200

    add background !

--- src/main/java/info3/game/menu/GamePanel.java
+++ src/main/java/info3/game/menu/GamePanel.java
@@ -2,7 +2,9 @@ package info3.game.menu;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
@@ -13,18 +15,22 @@ public class GamePanel extends JPanel {
 	private static final long serialVersionUID = -2703227226283234010L;
 	private MouseInputs mouseInputs;
 	private GameJerem game;
+	private Image bg;
 
 	public GamePanel(GameJerem game2) {
 		mouseInputs = new MouseInputs(this);
 		this.game = game2;
+		setOpaque(false);
 		setPanelSize();
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
+		this.bg = new ImageIcon("src/main/resources/UI/test.jpg").getImage();
 	}
 
 	private void setPanelSize() {
 		Dimension size = new Dimension(game.getGameWidth(), game.getGameHeight());
 		setPreferredSize(size);
+
 	}
 
 	public void updateGame() {
@@ -32,7 +38,7 @@ public class GamePanel extends JPanel {
 	}
 
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		g.drawImage(bg, 0, 0, this.getWidth(), this.getHeight(), null);
 		game.render(g);
 	}
 
