99e914f6314c68fb6276d2e23323b23c01015497
Lucas <lucas.mathieu1@etu.univ-grenoble-alpes.fr>
2022-06-24 13:15:23 +0200

    Implemented mushroom animations

--- src/main/java/info3/game/entities/Mushroom.java
+++ src/main/java/info3/game/entities/Mushroom.java
@@ -3,8 +3,10 @@ package info3.game.entities;
 import info3.game.LocalController;
 import info3.game.Model;
 import info3.game.Vec2;
+import info3.game.assets.AnimatedImage;
 import info3.game.assets.Image;
 import info3.game.automata.Category;
+import info3.game.automata.Direction;
 import info3.game.automata.behaviors.MushroomBehaviour;
 import info3.game.physics.RigidBody;
 
@@ -28,15 +30,29 @@ public class Mushroom extends RigidBody {
 		this.setAutomata(Model.getAutomata("Mushroom"));
 		this.setBehaviour(new MushroomBehaviour());
 		this.degatMob = 1;
+		this.avatarOffset = new Vec2(0, 0);
 		this.avatar = this.controller.createAvatar(new Vec2(this.getPosition()), new Image(this.avatarPath()));
+		this.playAnimation("spawn-right", 4, 100, 0, -10, false);
 	}
 
 	@Override
 	public void tick(long el) {
 		super.tick(el);
+		AnimatedImage anim = (AnimatedImage) this.getPaintable();
+		if (anim.isFinished()) {
+			if (this.getDirection() == Direction.EST)
+				this.playAnimation("idle-right", 4, 200, 0, -64, true);
+			else
+				this.playAnimation("idle-left", 4, 200, 0, -64, true);
+		}
 	}
 
 	private String avatarPath() {
-		return "mole-violet.png";
+		return "mushroom/idle-right.png";
+	}
+
+	@Override
+	public String animationDir() {
+		return "mushroom/";
 	}
 }
