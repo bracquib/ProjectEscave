c132886435b2784ddea77f149f359184df066514
Ana Gelez <ana@gelez.xyz>
2022-06-24 12:22:58 +0200

    Fix color names

--- src/main/resources/player/recolor.sh
+++ src/main/resources/player/recolor.sh
@@ -5,8 +5,8 @@ hue[vert]=30,240,30
 hue[bleu]=80,110,230
 hue[rouge]=210,50,50
 hue[violet]=180,50,190
-hue[black]=30,30,30
-hue[white]=248,248,248
+hue[noir]=30,30,30
+hue[blanc]=248,248,248
 hue[orange]=230,120,20
 
 for color in "${!hue[@]}"; do
